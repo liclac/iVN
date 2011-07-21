d0fa081e3e2afce02d526a69a55016ed32bf4078
Johannes Ekberg <uppfinnarn@gmail.com>
2011-07-21 14:49:33 +0200

    Initial Commit

---
+++ iVN/iVNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iVNAppDelegate.h
+//  iVN
+//
+//  Created by Johannes Ekberg on 2011-07-21.
+//  Copyright 2011 MacaroniCode Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iVNAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ iVN/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iVN
+//
+//  Created by Johannes Ekberg on 2011-07-21.
+//  Copyright 2011 MacaroniCode Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
